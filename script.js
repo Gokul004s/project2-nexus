9ff0d7ce0edfcd1f5e771ceb2dd0e23954f6cc51
Gokul <140362819+Gokul004s@users.noreply.github.com>
2024-02-11 21:34:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ window.addEventListener("scroll",function()
     header.classList.toggle("sticky",window.scrollY > 0 )
 })
 
+function showSidebar(){
+    const sidebar=document.querySelector('.side-bar')
+    sidebar.style.display='flex'
+    
+    const navitems=document.querySelector('.nav .menubtn')
+    navitems.style.display='none'
+}
 
+function hideSidebar(){
+    const hidebar=document.querySelector('.side-bar')
+    hidebar.style.display='none'
+    const navitems=document.querySelector('.nav .menubtn')
+    navitems.style.display='flex'
+}
